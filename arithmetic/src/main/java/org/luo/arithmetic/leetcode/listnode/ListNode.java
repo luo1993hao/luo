00309f5ff170163e6b57af0e99db3391c6b6755c
luohaoyu <283858831@qq.com>
2022-12-07 17:48:16 +0800

    add leetcode 142

---
+++ arithmetic/src/main/java/org/luo/arithmetic/leetcode/listnode/ListNode.java
@@ -0,0 +1,20 @@
+package org.luo.arithmetic.leetcode.listnode;
+
+import org.luo.arithmetic.leetcode.MatrixFound;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
