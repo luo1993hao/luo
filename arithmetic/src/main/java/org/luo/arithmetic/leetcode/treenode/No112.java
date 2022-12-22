74307e57757ca7d012ca63b14588caffb529b246
luohaoyu <283858831@qq.com>
2022-12-22 19:41:20 +0800

    add leetcode 112&&113&&257

--- arithmetic/src/main/java/org/luo/arithmetic/leetcode/treenode/No112.java
+++ arithmetic/src/main/java/org/luo/arithmetic/leetcode/treenode/No112.java
@@ -1,10 +1,17 @@
 package org.luo.arithmetic.leetcode.treenode;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class No112 {
 
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+
     /**
-     * dfs
+     * 112
      */
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) {
@@ -16,4 +23,60 @@ public class No112 {
         return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
 
     }
+
+    /**
+     * 113题
+     */
+    public void dfs(TreeNode root, int targetSum) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.val);
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            result.add(new ArrayList<>(path));
+        }
+        dfs(root.left, targetSum - root.val);
+        dfs(root.right, targetSum - root.val);
+        path.remove(path.size() - 1);
+    }
+
+    /**
+     * 257题
+     *
+     * @param root
+     * @return
+     */
+
+    List<String> res = new ArrayList<>();
+    List<String> pa = new ArrayList<>();
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        dfs1(root);
+        return res;
+    }
+
+    public void dfs1(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        pa.add(String.valueOf(root.val));
+        if (root.left == null && root.right == null) {
+            StringBuilder pc = new StringBuilder();
+            for (int i = 0; i < pa.size(); i++) {
+                if (i == 0) {
+                    pc = new StringBuilder(pa.get(i));
+                } else {
+                    pc.append("->").append(pa.get(i));
+                }
+            }
+            res.add(pc.toString());
+        }
+        dfs1(root.right);
+        dfs1(root.left);
+        pa.remove(pa.size() - 1);
+
+    }
+
+
+
 }
